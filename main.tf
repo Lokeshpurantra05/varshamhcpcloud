cb005b9abe3e8b78350ade4ea3c6722d9c1c1856
Lokeshpurantra05 <lokeshpurantra700@gmail.com>
2025-11-21 08:53:48 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,5 +5,7 @@ provider "aws" {
 resource "aws_instance" "one" {
   ami           = "ami-0fa3fe0fa7920f68e" # Example AMI ID, replace with a valid one
   instance_type = "t2.micro"              # Default instance type, can be overridden
-  tags          = var.tags
+  tags = {
+    Name = "lokesh-server"
+  }
 }
