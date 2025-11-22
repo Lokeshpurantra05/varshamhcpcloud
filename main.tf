270f4723a34b59c05975def7b9498471bba412ff
Lokeshpurantra05 <Lokeshpurantra700@gmail.com>
2025-11-22 07:04:29 -0800

    add

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "one" {
   ami           = "ami-03978d951b279ec0b" # Example AMI ID, replace with a valid one
   instance_type = "t2.micro"              # Default instance type, can be overridden
   tags = {
-    Name = "lokesh-server"
+    Name = "lokesh-server1"
   }
 }
